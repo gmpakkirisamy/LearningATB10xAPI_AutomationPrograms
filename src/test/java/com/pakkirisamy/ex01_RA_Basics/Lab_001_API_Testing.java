c9bb020057d40ae7aea23208b7dfc20f5cf511f5
Pakkirisamy.M <gmpakkirisamy@gmail.com>
2025-03-18 18:49:21 +0530

    18_March_Part_1

---
+++ src/test/java/com/pakkirisamy/ex01_RA_Basics/Lab_001_API_Testing.java
@@ -0,0 +1,23 @@
+package com.pakkirisamy.ex01_RA_Basics;
+
+import io.restassured.RestAssured;
+
+public class Lab_001_API_Testing {
+    public static void main(String[] args) {
+    //Gherkins Syntax
+        //Given() -> Pre Req - URL, Headers, Auth, Body....
+        //When() -> HTTP method ? - GET/POST/PUT/PATCH/DELETE....
+        //Then() -> Validation -> 200 ok, firstname == SAMY
+
+        //Full URL - https://api.zippopotam.us/IN/560016
+        //Base URL - https://api.zippopotam.us
+        //Base Path -/IN/560016
+
+        RestAssured.
+                given()
+                .baseUri("https://api.zippopotam.us")
+                .basePath("/IN/560016").when()
+                .get()
+                .then().log().all().statusCode(200);
+    }
+}
