49f2eac556892f578c81517a3aed1e57fd09e7a8
Pakkirisamy.M <gmpakkirisamy@gmail.com>
2025-03-19 18:39:46 +0530

    19_March_Part_1

--- src/test/java/com/pakkirisamy/ex01_RA_Basics/Lab_001_API_Testing.java
+++ src/test/java/com/pakkirisamy/ex01_RA_Basics/Lab_001_API_Testing.java
@@ -6,18 +6,7 @@ import io.restassured.config.SSLConfig;
 public class Lab_001_API_Testing {
     public static void main(String[] args) {
 
-        RestAssured.config = RestAssured.config().sslConfig(SSLConfig.sslConfig().allowAllHostnames());
-        RestAssured.
-                given()
-                .baseUri("https://api.zippopotam.us")
-                .basePath("/IN/560016")
-                .when()
-                .get()
-                .then()
-                .log().all()
-                .statusCode(200);
-        /// /
-    //Gherkins Syntax
+        //Gherkins Syntax
         //Given() -> Pre Req - URL, Headers, Auth, Body....
         //When() -> HTTP method ? - GET/POST/PUT/PATCH/DELETE....
         //Then() -> Validation -> 200 ok, firstname == SAMY
@@ -26,18 +15,17 @@ public class Lab_001_API_Testing {
         //Base URI  - https://api.zippopotam.us
         //Base Path -/IN/560016
 
-//        RestAssured.
-//                given()
-//                .useRelaxedHTTPSValidation()
-//                .when()
-//                .get("http://abcd.com/resource");
+        RestAssured.config = RestAssured.config().sslConfig(SSLConfig.sslConfig().allowAllHostnames());
+        RestAssured.config = RestAssured.config().sslConfig(SSLConfig.sslConfig().relaxedHTTPSValidation());
+        RestAssured.
+                given()
+                    .baseUri("https://api.zippopotam.us")
+                    .basePath("/IN/560016")
+                .when()
+                    .get()
+                .then()
+                    .log().all()
+                .statusCode(200);
 
-//                RestAssured.
-//                given()
-//                .baseUri("https://api.zippopotam.us")
-//                .basePath("/IN/560016").when()
-//                .get()
-//                .then().log().all().statusCode(200);
-//        System.out.print("hi");
     }
 }
