b6ebd3af7a0b1401bac85bdfa435943340a5bcec
Pakkirisamy.M <gmpakkirisamy@gmail.com>
2025-03-19 14:09:47 +0530

    18_March_Part_1: changed by samy: Regarding

--- src/test/java/com/pakkirisamy/ex01_RA_Basics/Lab_001_API_Testing.java
+++ src/test/java/com/pakkirisamy/ex01_RA_Basics/Lab_001_API_Testing.java
@@ -1,9 +1,21 @@
 package com.pakkirisamy.ex01_RA_Basics;
 
 import io.restassured.RestAssured;
+import io.restassured.config.SSLConfig;
 
 public class Lab_001_API_Testing {
     public static void main(String[] args) {
+
+        RestAssured.config = RestAssured.config().sslConfig(SSLConfig.sslConfig().allowAllHostnames());
+        RestAssured.
+                given()
+                .baseUri("https://api.zippopotam.us")
+                .basePath("/IN/560016")
+                .when()
+                .get()
+                .then()
+                .log().all()
+                .statusCode(200);
     //Gherkins Syntax
         //Given() -> Pre Req - URL, Headers, Auth, Body....
         //When() -> HTTP method ? - GET/POST/PUT/PATCH/DELETE....
@@ -13,11 +25,11 @@ public class Lab_001_API_Testing {
         //Base URI  - https://api.zippopotam.us
         //Base Path -/IN/560016
 
-        RestAssured.
-                given()
-                .useRelaxedHTTPSValidation()
-                .when()
-                .get("http://abcd.com/resource");
+//        RestAssured.
+//                given()
+//                .useRelaxedHTTPSValidation()
+//                .when()
+//                .get("http://abcd.com/resource");
 
 //                RestAssured.
 //                given()
@@ -25,5 +37,6 @@ public class Lab_001_API_Testing {
 //                .basePath("/IN/560016").when()
 //                .get()
 //                .then().log().all().statusCode(200);
+//        System.out.print("hi");
     }
 }
