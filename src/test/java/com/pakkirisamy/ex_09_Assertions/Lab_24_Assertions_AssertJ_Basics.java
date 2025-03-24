081b02496adbed82d57606732e7b0b4abe994d03
Pakkirisamy.M <gmpakkirisamy@gmail.com>
2025-03-24 17:46:11 +0530

    24_March_API_Part_1

---
+++ src/test/java/com/pakkirisamy/ex_09_Assertions/Lab_24_Assertions_AssertJ_Basics.java
@@ -0,0 +1,64 @@
+package com.pakkirisamy.ex_09_Assertions;
+
+import com.sun.net.httpserver.Request;
+import io.restassured.RestAssured;
+import io.restassured.config.SSLConfig;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+import io.restassured.specification.RequestSpecification;
+import org.testng.annotations.Test;
+import static org.assertj.core.api.Assertions.*;
+
+public class Lab_24_Assertions_AssertJ_Basics {
+
+    RequestSpecification requestSpecification;
+    ValidatableResponse validatableResponse;
+    Response response;
+    String token;
+    Integer bookingId;
+
+    @Test
+    public void test_POST() {
+        RestAssured.config = RestAssured.config().sslConfig(SSLConfig.sslConfig().allowAllHostnames().relaxedHTTPSValidation());
+        String payload_POST = "{\n" +
+                "    \"bookingid\": 832,\n" +
+                "    \"booking\": {\n" +
+                "        \"firstname\": \"Faye\",\n" +
+                "        \"lastname\": \"Hamill\",\n" +
+                "        \"totalprice\": 111,\n" +
+                "        \"depositpaid\": true,\n" +
+                "        \"bookingdates\": {\n" +
+                "            \"checkin\": \"2018-01-01\",\n" +
+                "            \"checkout\": \"2019-01-01\"\n" +
+                "        },\n" +
+                "        \"additionalneeds\": \"Breakfast\"\n" +
+                "    }\n" +
+                "}";
+
+        requestSpecification = RestAssured.given();
+        requestSpecification.baseUri("https://restful-booker.herokuapp.com");
+        requestSpecification.basePath("/booking");
+        requestSpecification.contentType(ContentType.JSON);
+        requestSpecification.body(payload_POST).log().all();
+
+        Response response = requestSpecification.when().post();
+
+
+        //Get Validate response to perform the validation.
+
+        validatableResponse = response.then().log().all();
+        validatableResponse.statusCode(200);
+
+        bookingId = response.then().extract().path("bookingid");
+        String firstname = response.then().extract().path("booking.firstname");
+        String lastname = response.then().extract().path("booking.lastname");
+
+        //AssertJ (3rd Lib to Assertions)
+
+        assertThat(bookingId).isNotNull().isNotZero().isPositive();
+        assertThat(firstname).isEqualTo("Faye").isNotEmpty().isNotBlank().isNotNull();
+
+
+    }
+}
